af44b390bc9963094588eb6b7ede1508e4481c07
SincereSaksham <135314389+SincereSaksham@users.noreply.github.com>
2025-06-08 03:39:36 +0200

    Update app.py
    Changed @app.before_first_request, because its not available in flask 3.1.1

--- PulseCheck/app.py
+++ PulseCheck/app.py
@@ -428,11 +428,8 @@ def logout():
 
 
 
-@app.before_first_request
-def create_tables():
-    with app.app_context():
-        db.create_all()
-
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
